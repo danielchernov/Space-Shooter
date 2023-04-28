fc5d99d32ded2e03a8b5e47882fca9ea98e69d4f
danielchernov <75706332+danielchernov@users.noreply.github.com>
2023-04-28 19:12:43 -0300

    Pause and Main Menus, Polishing
    Last commit before publishing.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,16 +9,45 @@ public class GameManager : MonoBehaviour
 
     public bool IsCoopMode = false;
 
+    private int currentScene;
+
+    public GameObject pauseMenu;
+
+    public Animator pauseMenuAnimator;
+
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        if (pauseMenu != null)
+        {
+            pauseMenuAnimator = pauseMenu.GetComponent<Animator>();
+        }
+    }
+
     void Update()
     {
         if (_isGameOver && Input.GetKeyDown(KeyCode.R))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadScene(currentScene);
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Application.Quit();
+            if (currentScene != 0)
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+            else if (currentScene == 0)
+            {
+                Application.Quit();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.P) && !_isGameOver && currentScene != 0)
+        {
+            Time.timeScale = 0;
+            pauseMenuAnimator.SetBool("isPaused", true);
+            //pauseMenu.SetActive(true);
         }
     }
 
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField]
     private Text _scoreText;
+
+    [SerializeField]
+    private Text _bestScoreText;
 
     [SerializeField]
     private Sprite[] _liveSprites;
@@ -19,9 +23,19 @@ public class UIManager : MonoBehaviour
 
     private GameManager _gameManager;
 
+    private int _score = 0;
+    private int _bestScore = 0;
+
     void Start()
     {
+        _bestScore = PlayerPrefs.GetInt("BestScore", 0);
         _scoreText.text = "Score: " + 0;
+
+        if (_bestScoreText != null)
+        {
+            _bestScoreText.text = "Best: " + _bestScore;
+        }
+
         _gameOver.SetActive(false);
         _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 
@@ -33,7 +47,21 @@ public class UIManager : MonoBehaviour
 
     public void UpdateScore(int score)
     {
+        _score = score;
         _scoreText.text = "Score: " + score;
+    }
+
+    public void CheckForBestScore()
+    {
+        if (_bestScoreText != null)
+        {
+            if (_score > _bestScore)
+            {
+                _bestScore = _score;
+                _bestScoreText.text = "Best: " + _bestScore;
+                PlayerPrefs.SetInt("BestScore", _bestScore);
+            }
+        }
     }
 
     public void UpdateLives(int lives)
@@ -44,6 +72,20 @@ public class UIManager : MonoBehaviour
         {
             _gameOver.SetActive(true);
             _gameManager.GameOver();
+            CheckForBestScore();
         }
+    }
+
+    public void ResumePlay()
+    {
+        Time.timeScale = 1;
+        _gameManager.pauseMenuAnimator.SetBool("isPaused", false);
+        ;
+    }
+
+    public void QuitToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
     }
 }
